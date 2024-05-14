eeb54831d85f4eb16f66dfa25a6bdf4c96feedbd
Your Name <dulneth.bernard@gmail.com>
2024-05-14 17:11:56 +0530

    Traditional way of fetching data using SWR method

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 
+import axios from "axios";
+import { useEffect,useState } from "react";
 
 function App() {
+  const[data,setData]=useState([]);
 
+  useEffect(()=>{
+    axios.get('http://localhost:8080/todos').then(response => {
+      setData(response.data)}).catch((error)=>{
+        console.log(error);
+      });
+
+  },[]);
+
+
+ 
 
 
 
    return (
     <>
-      hi
+      {JSON.stringify(data)}
     </>
    );
    
